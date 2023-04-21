eed05ded891bfa19d570f1aa9eccb8c5632d4fae
kalwhyte <superkalel55@gmail.com>
2023-04-21 11:40:13 +0000

    A Puppet manifest that creates the file /tmp/school

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Using Puppet, create a file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
