f001b500a98cea60b14858cf67975e283f8daa2f
kalwhyte <superkalel55@gmail.com>
2023-04-23 04:28:31 +0100

    system engineering devops

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#configure ssh config
+
+file_line{'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
+}
+
+file_line{'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school'
+}
