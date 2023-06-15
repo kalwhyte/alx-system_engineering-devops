583d635929b446e4515d0fbca79f08eb4484251e
kalwhyte <superkalel55@gmail.com>
2023-06-15 13:08:06 +0000

    simulate HTTP requests to a web server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This increases the amount of traffic an Nginx server can handle
 
-# Increasing the ULIMIT of the default file
+# This increases the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
